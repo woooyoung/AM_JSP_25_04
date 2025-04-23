d62626197239ea7e44973533a0905d5ed8b57c54
woooyoung <axdsw121@gmail.com>
2025-04-23 15:52:07 +0900

    파라미터 사용

--- src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/AM_jsp/PrintDanServlet.java
@@ -15,15 +15,26 @@ public class PrintDanServlet extends HttpServlet {
 
 		response.setContentType("text/html;charset=UTF-8");
 
-		response.getWriter().append("==8단==<br>");
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
 
-//		response.getWriter().append("8 * 1 = 8\n");
-//		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 1, 8));
-//		response.getWriter().append(String.format("%d * %d = %d<br>", 8, 2, 16));
+		if (inputedDan == null) {
+			inputedDan = "1";
+		}
 
-		int dan = 8;
+		if (inputedLimit == null) {
+			inputedLimit = "1";
+		}
 
-		for (int i = 1; i <= 9; i++) {
+		System.out.println(inputedDan);
+		System.out.println(inputedLimit);
+
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+
+		for (int i = 1; i <= limit; i++) {
 			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
 		}
 
